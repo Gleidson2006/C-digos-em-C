f94f3c860e09c5d5a280b733f1d578f1000770bb
Gleidson2006 <gleidson.fraga@sempreceub.com>
2026-05-19 12:25:35 -0300

    Add files via upload

---
+++ Lista de exercícios.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+
+int main(){
+	char nome [8];
+	printf("Digite seu nome: ");
+	scanf("%s", &nome);
+	printf("Olá %s, bem vindo ao curso de Ciência da Computação",nome);
+	return 0;
+}
