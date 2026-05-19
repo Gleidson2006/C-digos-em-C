9f0a0e9ab4699066f0fe27eb45590532cde1e227
Gleidson2006 <gleidson.fraga@sempreceub.com>
2026-05-19 12:28:35 -0300

    Add files via upload

---
+++ progressivaERegressiva.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(){
+	int numero;
+	printf("\nDigite um numero:");
+	scanf("%d",&numero);
+	printf("Antecessor: %d \n", numero - 1);
+	printf ("sucessor: %d",numero +1);
+
+
+	return 0;
+}
